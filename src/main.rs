a2ad22b2738fcb44144c9921cc8b6c5c07375133
Random Long Int <pierrelouislet@gmail.com>
2024-01-29 18:13:57 -0500

    minor upgrades

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,51 @@ const HEIGHT: usize = 256;
 
 fn main() {
     let file_path = env::args().nth(1).expect("No file path provided");
-    let out_path = env::args().nth(2).expect("No output path provided");
 
-    let bytes = fs::read(file_path).expect("Failed to read file");
+    let bytes = fs::read(&file_path).expect("Failed to read file");
 
-    let mut map: Vec<Vec<u8>> = vec![vec![0; WIDTH]; HEIGHT];
+    let mut map: Vec<Vec<u32>> = vec![vec![0; WIDTH]; HEIGHT];
+
+    println!("Processing file: {}", file_path);
+    println!("Bytes: {}", bytes.len());
 
     for window in bytes.windows(2) {
         let x = window[0] as usize;
         let y = window[1] as usize;
-        if map[y][x] == 255 {
-            continue;
-        }
         map[y][x] += 1;
     }
 
-    let mut file: fs::File = fs::File::create(out_path).expect("Failed to create file");
-
-    let _ = file.write_all(&format!("P6\n256 256\n255\n").as_bytes());
-
-    let mut temp: Vec<u8> = Vec::with_capacity(WIDTH * HEIGHT * 3);
+    let mut max = 0.0;
 
     map.iter().for_each(|row| {
         row.iter().for_each(|v| {
-            temp.push(*v);
-            temp.push(*v);
-            temp.push(*v);
+            let f: f32 = if *v > 0 { (*v as f32).log10() } else { 0.0 };
+            if f > max {
+                max = f;
+            }
         })
     });
 
-    let _ = file.write_all(&temp);
+    let mut pixels: Vec<Vec<u32>> = vec![vec![0; WIDTH]; HEIGHT];
+
+    pixels.iter_mut().enumerate().for_each(|(y, row)| {
+        row.iter_mut().enumerate().for_each(|(x, v)| {
+            let t = map[y][x] as f32 / max;
+            let b = (t * 255.0) as u32;
+            *v = b;
+        })
+    });
+
+    let mut file: fs::File = fs::File::create("outputs/out.ppm").expect("Failed to create file");
+
+    let _ = file.write_all(&format!("P6\n256 256\n255\n").as_bytes());
+
+    pixels.iter().for_each(|row| {
+        row.iter().for_each(|v| {
+            let b = *v as u8;
+            let _ = file.write_all(&[b, b, b]);
+        })
+    });
+
+    println!("Output written to outputs/out.ppm");
 }
