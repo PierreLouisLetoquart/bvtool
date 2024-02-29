33701995f3e92d1c56d42e651fb63254b9faf4f4
Pierre-Louis Létoquart <pierrelouislet@gmail.com>
2024-02-28 19:03:50 -0500

    feat: CLI

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,11 @@ use na::SMatrix;
 use std::fs::File;
 use std::io::Read;
 
-pub const SIZE: usize = 256;
+const SIZE: usize = 256;
+
 pub type Map256 = SMatrix<u32, SIZE, SIZE>;
 
-pub fn load_file(file_path: &str) -> Vec<u8> {
+pub fn load_file(file_path: &std::path::PathBuf) -> Vec<u8> {
     let mut file = File::open(file_path).expect("Can't open that file");
     let mut slice = Vec::new();
     file.read_to_end(&mut slice).unwrap();
@@ -54,7 +55,11 @@ pub fn map_to_image(map: &Map256) -> RgbImage {
     let mut img: RgbImage = ImageBuffer::new(SIZE as u32, SIZE as u32);
     for i in 0..SIZE {
         for j in 0..SIZE {
-            img.put_pixel(i as u32, j as u32, image::Rgb([0, map[(i, j)] as u8, 0]));
+            img.put_pixel(
+                i as u32,
+                j as u32,
+                image::Rgb([map[(i, j)] as u8, map[(i, j)] as u8, map[(i, j)] as u8]),
+            );
         }
     }
     img
--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,25 @@
 use bvtool::{generate_visualization, load_file, map_to_image, Map256};
-use clap::{Parser, ValueEnum};
+use clap::Parser;
 use image::RgbImage;
-use serde::Serialize;
-
-// Possibilities
-//
-// generate a heatmap of the entire file
-// save the heatmap as a .png file (default to data/heatmap.png)
-// save the heatmap as a .png file in a custom path
-// specify the color of the heatmap (default to white)
-// generate a heatmap for all the files in a directory
-
-#[derive(ValueEnum, Clone, Default, Debug, Serialize)]
-#[serde(rename_all = "kebab-case")]
-enum Colors {
-    #[default]
-    White,
-    Red,
-    Green,
-    Blue,
-    Yellow,
-    Cyan,
-    Magenta,
-}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    path: Option<std::path::PathBuf>,
+    path: std::path::PathBuf,
 
     #[arg(short, long, default_value = "data/heatmap.png")]
     output: std::path::PathBuf,
-
-    #[arg(short, long)]
-    dir: Option<String>,
-
-    #[arg(short, long, default_value_t, value_enum)]
-    color: Colors,
-
-    #[arg(short, long, default_value = "false")]
-    quiet: bool,
 }
 
 fn main() {
     let args = Args::parse();
 
-    if let Some(path) = args.path {
-        println!("Loading file: {:?}", path);
-    } else if let Some(dir) = args.dir {
-        println!("Loading directory: {:?}", dir);
-    } else {
-        eprintln!("No path or directory specified");
-        println!("run `bvtool --help` for more information");
-        std::process::exit(1);
-    }
+    let slice = load_file(&args.path);
+    let mut map = Map256::zeros();
 
-    // get the path of all the files in "path"
-    // let mut limit = 0;
-    // let paths = std::fs::read_dir("").unwrap();
-    // for entry in paths {
-    //     let entry = entry.unwrap();
-    //     let file_path = entry.path();
-    //     let file_name = file_path.file_name().unwrap().to_str().unwrap();
+    generate_visualization(&slice, &mut map);
+    let img: RgbImage = map_to_image(&map);
 
-    //     println!("[LOADING] {}", file_name);
-    //     let slice = load_file(&file_path.to_str().unwrap());
-    //     let mut map = Map256::zeros();
-
-    //     println!("[GENERATING] generating 256x256 map");
-    //     generate_visualization(&slice, &mut map);
-    //     let img: RgbImage = map_to_image(&map);
-
-    //     let out_path = format!("data/img-jpg/{}.bvtool.png", file_name);
-    //     println!("[DONE] saving map into {}", &out_path);
-    //     img.save(out_path).unwrap();
-    //     limit += 1;
-    //     if limit == 8000 {
-    //         // finish program
-    //         break;
-    //     }
-
-    // // Open target file
-    // let file_path = "./target/debug/bvtool";
-    // let slice = load_file(&file_path);
-
-    // // Create the 256x256 map
-    // let mut map = Map256::zeros();
-
-    // // Generate the visualization
-    // generate_visualization(&slice, &mut map);
-
-    // // Create the image
-    // let img: RgbImage = map_to_image(&map);
-
-    // // Save the image
-    // img.save("data/heatmap.png").unwrap();
-}
-
-fn _gen_exec_viz() {
-    let paths = std::env::var("PATH").unwrap();
-    let paths: Vec<&str> = paths.split(':').collect();
-    for path in paths {
-        let dir = std::fs::read_dir(path).unwrap();
-        for entry in dir {
-            let entry = entry.unwrap();
-            let file_path = entry.path();
-            let file_name = file_path.file_name().unwrap().to_str().unwrap();
-
-            println!("[LOADING] {}", file_name);
-            let slice = load_file(&file_path.to_str().unwrap());
-            let mut map = Map256::zeros();
-
-            println!("[GENERATING] generating 256x256 map");
-            generate_visualization(&slice, &mut map);
-            let img: RgbImage = map_to_image(&map);
-
-            let out_path = format!("data/exec/{}.bvtool.png", file_name);
-            println!("[DONE] saving map into {}", &out_path);
-            img.save(out_path).unwrap();
-        }
-    }
+    img.save(&args.output).unwrap();
 }
