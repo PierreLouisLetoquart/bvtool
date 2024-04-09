822b71c0491c736aea82bdf8b775353f446b3895
Pierre-Louis Létoquart <pierrelouislet@gmail.com>
2024-04-09 17:21:20 -0400

    chore(tests-4-pres): recursive img generation

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,27 @@ fn main() {
     let img: RgbImage = map_to_image(&map);
 
     img.save(&args.output).unwrap();
+    // recursive();
 }
+
+// fn recursive() {
+//     let dir = "/Users/pilou/Coding/binviz/bvtool/data/speech/Actor_01";
+//     let dir_out = "/Users/pilou/Coding/binviz/bvtool/data/speech";
+//
+//     let files = std::fs::read_dir(dir).unwrap();
+//
+//     for file in files {
+//         let file = file.unwrap();
+//         let path = file.path();
+//         let name = path.file_name().unwrap().to_str().unwrap();
+//         let out = format!("{}/actor_1_{}.png", dir_out, name);
+//
+//         let slice = load_file(&path);
+//         let mut map = Map256::zeros();
+//
+//         generate_visualization(&slice, &mut map);
+//         let img: RgbImage = map_to_image(&map);
+//
+//         img.save(&out).unwrap();
+//     }
+// }
